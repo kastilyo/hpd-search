9346a39388f8dbe2fa8a4a302f3f2653d9ba48bb
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-04 17:33:48 -0400

    Adding support for parsing soda violations

--- src/nycOpenData/sodaStream/resourceIds.js
+++ src/nycOpenData/sodaStream/resourceIds.js
@@ -2,4 +2,5 @@ const TYPES = require('./types');
 
 module.exports = {
   [TYPES.LITIGATION]: 'kfyu-46k5',
+  [TYPES.VIOLATION]: 'b2iz-pps8',
 };
--- src/nycOpenData/sodaStream/types.js
+++ src/nycOpenData/sodaStream/types.js
@@ -1,3 +1,4 @@
 module.exports = {
   LITIGATION: 'litigation',
+  VIOLATION: 'violation',
 };

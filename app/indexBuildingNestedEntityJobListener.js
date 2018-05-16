dfaf928f6b39af9478d712543c6c715ad61e0e64
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-15 21:55:29 -0400

    DRYing

---
+++ app/indexBuildingNestedEntityJobListener.js
@@ -0,0 +1,85 @@
+/* eslint-disable no-console */
+const Bacon = require('baconjs');
+
+const { RabbitHole } = require('./bootstrap');
+
+const { Building } = require('./../src/bulkOperation');
+
+const TYPES = {
+  VIOLATION: 'violation',
+  COMPLAINT: 'complaint',
+};
+
+const resolveQueue =
+  type => {
+    switch (type) {
+    case TYPES.VIOLATION:
+      return process.env.RABBIT_HOLE_INDEX_BUILDING_VIOLATION_QUEUE;
+    case TYPES.COMPLAINT:
+      return process.env.RABBIT_HOLE_INDEX_BUILDING_COMPLAINT_QUEUE;
+    }
+  };
+
+const resolveBulkOperationBuilder =
+  type => {
+    switch(type) {
+    case TYPES.VIOLATION:
+      return Building.upsertViolations;
+    case TYPES.COMPLAINT:
+      return Building.upsertComplaints;
+    }
+  };
+
+const groupEntitiesByBuildingId =
+  messages =>
+    messages.reduce(
+      (groupedEntities, { json: { data } }) =>
+        ({
+          ...groupedEntities,
+          [data.buildingId]: !groupedEntities[data.buildingId]
+            ? [data]
+            : [...groupedEntities[data.buildingId], data]
+        }),
+      {}
+    );
+
+const create =
+  ({
+    type,
+    buffer: { size, timeoutMs }
+  }) => {
+    const bulkOperationBuilder = resolveBulkOperationBuilder(type);
+    RabbitHole.create().then(rabbitHole => Promise.all([
+      rabbitHole.createJsonPublisher(process.env.RABBIT_HOLE_EXCHANGE),
+      rabbitHole.createJsonConsumer(resolveQueue(type)),
+      rabbitHole,
+    ])).then(([publisher, consumer, rabbitHole]) => {
+      console.log('Listening...');
+
+      process.once('SIGINT', () => {
+        console.log('Closing...');
+        rabbitHole.close();
+      });
+
+      const messageStream = Bacon
+        .fromBinder(sink => consumer.consume(({ message }) => sink(message)))
+        .bufferWithTimeOrCount(timeoutMs, size);
+
+      messageStream.onValue(messages => {
+        const entitiesByBuildingId = groupEntitiesByBuildingId(messages);
+
+        const bulkOperations = Object
+          .keys(entitiesByBuildingId)
+          .map(buildingId => bulkOperationBuilder(buildingId, entitiesByBuildingId[buildingId]));
+
+        Promise.all(bulkOperations.map(bulkOperation => publisher.publish('index', {
+          operation: bulkOperation,
+        }))).then(() => Promise.all(messages.map(consumer.ack)));
+      });
+    });
+  };
+
+module.exports = {
+  create,
+  TYPES,
+};
--- app/indexBuildingViolationJobListener.js
+++ app/indexBuildingViolationJobListener.js
@@ -1,51 +1,10 @@
 /* eslint-disable no-console */
-const Bacon = require('baconjs');
+const IndexBuildingNestedEntityJobListener = require('./indexBuildingNestedEntityJobListener');
 
-const { RabbitHole } = require('./bootstrap');
-
-const BulkOperation = require('./../src/bulkOperation');
-
-const BATCH_SIZE = 20000;
-const BUFFER_TIMEOUT_MS = 10000;
-
-RabbitHole.create().then(rabbitHole => Promise.all([
-  rabbitHole.createJsonPublisher(process.env.RABBIT_HOLE_EXCHANGE),
-  rabbitHole.createJsonConsumer(process.env.RABBIT_HOLE_INDEX_BUILDING_COMPLAINT_QUEUE, { prefetch: BATCH_SIZE }),
-  rabbitHole,
-])).then(([publisher, consumer, rabbitHole]) => {
-  console.log('Listening...');
-
-  process.once('SIGINT', () => {
-    console.log('Closing...');
-    rabbitHole.close();
-  });
-
-  const messageStream = Bacon.fromBinder(sink => consumer.consume(({ message }) => sink(message)))
-    .bufferWithTimeOrCount(BUFFER_TIMEOUT_MS, BATCH_SIZE);
-
-  messageStream.onValue(messages => {
-    const violationsByBuildingId = messages.reduce(
-      (groupedItems, { json: { data } }) =>
-        ({
-          ...groupedItems,
-          [data.buildingId]: !groupedItems[data.buildingId]
-            ? [data]
-            : [...groupedItems[data.buildingId], data]
-        }),
-      {}
-    );
-
-    const bulkOperations = Object.keys(violationsByBuildingId)
-      .map(
-        buildingId =>
-          BulkOperation.Building.upsertViolations(
-            buildingId,
-            violationsByBuildingId[buildingId]
-          )
-      );
-
-    Promise.all(bulkOperations.map(bulkOperation => publisher.publish('index', {
-      operation: bulkOperation,
-    }))).then(() => Promise.all(messages.map(consumer.ack)));
-  });
+IndexBuildingNestedEntityJobListener.create({
+  type: IndexBuildingNestedEntityJobListener.TYPES.VIOLATION,
+  buffer: {
+    size: 20000,
+    timeoutMs: 10000,
+  }
 });

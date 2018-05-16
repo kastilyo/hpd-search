30fa33c130f84f34795d2349602a42c60b42c483
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-15 21:36:07 -0400

    No more repository

--- src/repository/building/operations/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  upsert: require('./upsert'),
-  upsertComplaint: require('./upsertComplaint'),
-  upsertViolation: require('./upsertViolation'),
-  upsertViolations: require('./upsertViolations'),
-};
--- src/repository/building/operations/upsert.js
+++
@@ -1,6 +0,0 @@
-module.exports =
-  building =>
-    [
-      { update: { _type: 'building', _id: building.id } },
-      { doc: building, doc_as_upsert: true },
-    ];

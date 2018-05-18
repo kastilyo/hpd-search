c4912d411e360c24730eee169a8f79c80d04ede9
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-17 20:22:13 -0400

    Support passing in json middleware options

--- app/bootstrap/dependencies/rabbitHole.js
+++ app/bootstrap/dependencies/rabbitHole.js
@@ -6,9 +6,9 @@ const create =
       vhost: process.env.RABBIT_HOLE_VHOST,
     }).then(rh => ({
       ...rh,
-      createJsonPublisher: exchange =>
+      createJsonPublisher: (exchange, jsonMiddlewareOptions = {}) =>
         rh.MiddlewarePublisher.create(exchange)
-          .then(publisher => publisher.use(RabbitHole.Middleware.Publisher.json({ JSON: require('circular-json') }))),
+          .then(publisher => publisher.use(RabbitHole.Middleware.Publisher.json(jsonMiddlewareOptions))),
       createJsonConsumer: (queue, options = {}) =>
         rh.MiddlewareConsumer.create(queue, options)
           .then(consumer => consumer.use(RabbitHole.Middleware.Consumer.json()))

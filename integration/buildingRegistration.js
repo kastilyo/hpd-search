28d880e20dc395086b1a1c71106ea96451913727
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-19 00:03:27 -0400

    Adding test script verifying registration data transformations

---
+++ integration/buildingRegistration.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const R = require('ramda')
+  , JSON = require('circular-json');
+
+const { NycOpenData } = require('./../app/bootstrap');
+
+const BulkOperation = require('./../src/bulkOperation')
+  , Domain = require('./../src/domain');
+
+NycOpenData
+  .create()
+  .getStream('xml', 'registration')
+  .onValue(({ data }) => console.log(JSON.stringify(data)))
+  .map(({ data }) => Domain.Registration.fromXml(data))
+  .onValue(data => console.log(JSON.stringify(data)))
+  .map(BulkOperation.Building.upsertRegistration)
+  .onValue(data => console.log(JSON.stringify(data)));

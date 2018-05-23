bfa304892e44286c70d5a8b467d1483064b9b9bf
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-22 20:35:45 -0400

    Forgot to omit buildingId here

--- src/bulkOperation/building/upsertRegistration.js
+++ src/bulkOperation/building/upsertRegistration.js
@@ -1,6 +1,8 @@
+const R = require('ramda');
+
 module.exports =
   registration =>
     [
       { update: { _type: 'building', _id: registration.buildingId } },
-      { doc: { registration }, doc_as_upsert: true },
+      { doc: { registration: R.omit(['buildingId'], registration) }, doc_as_upsert: true },
     ];

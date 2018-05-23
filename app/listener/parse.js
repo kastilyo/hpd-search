43ec48b776e55ac063803901759d7e686d515a46
Mark Castillo <markalexandercastillo@gmail.com>
2018-05-22 20:49:09 -0400

    Merging entity hydration in the parse listener
    Queues actually drain now

--- app/listener/parse.js
+++ app/listener/parse.js
@@ -1,6 +1,43 @@
 /* eslint-disable no-console */
 const { RabbitHole, NycOpenData } = require('./../bootstrap');
 
+const Domain = require('./../../src/domain');
+
+const hydrateEntity =
+  (type, source, data) =>
+    Domain[upperFirst(type)][`from${upperFirst(source)}`](data);
+
+const upperFirst =
+  str =>
+    str[0].toUpperCase() + str.slice(1);
+
+const getRoutingKeys =
+  type => {
+    switch (type) {
+    case 'building':
+      return [
+        'hydrate.bulk-upsert.building',
+        'geocode.building',
+      ];
+    case 'violation':
+      return [
+        'hydrate.bulk-upsert.building-violation',
+      ];
+    case 'complaint':
+      return [
+        'hydrate.bulk-upsert.building-complaint',
+      ];
+    case 'registration':
+      return [
+        'hydrate.bulk-upsert.building-registration',
+      ];
+    case 'litigation':
+      return [
+        'hydrate.bulk-upsert.building-litigation',
+      ];
+    }
+  };
+
 RabbitHole.create().then(rabbitHole => Promise.all([
   rabbitHole.createJsonPublisher(process.env.RABBIT_HOLE_EXCHANGE, { JSON: require('circular-json')}),
   rabbitHole.createJsonConsumer(process.env.RABBIT_HOLE_PARSE_QUEUE),
@@ -31,9 +68,28 @@ RabbitHole.create().then(rabbitHole => Promise.all([
     console.log(`Received message ${JSON.stringify(message.json)}`);
 
     const dataStream = nycOpenData.getStream(source, type, filter)
-      .doAction(() => `Parsed ${type} from ${source.toUpperCase()}`);
+      .map(
+        ({ type, source, data }) =>
+          ({
+            type,
+            source,
+            data: hydrateEntity(type, source, data)
+          }))
+      .doAction(
+        ({ type, source, data }) =>
+          `Parsed ${type} ID ${data.id} from ${source.toUpperCase()}`
+      );
 
-    dataStream.onValue(data => publisher.publish('hydrate.entity', data));
+    dataStream.onValue(
+      ({ data, type }) =>
+        Promise.all(
+          getRoutingKeys(type)
+            .map(
+              routingKey =>
+                publisher.publish(routingKey, { type, data })
+            )
+        )
+    );
 
     dataStream.onEnd(() => {
       console.log('Acking...');

18cbde1c7d487ebc0372c49cf0f00465399ef57f
Mark Castillo <markalexandercastillo@gmail.com>
2018-08-26 08:20:37 -0400

    Aggregations on some fields seem to follow this pattern. Generalizing and applying to terms.

--- src/elasticsearch/aggregations/index.js
+++ src/elasticsearch/aggregations/index.js
@@ -1,15 +1,15 @@
-const geoAggregation = require('./geoAggregation');
+const fieldAggregation = require('./fieldAggregation');
 
 module.exports = {
   /**
    * A metric aggregation that computes the bounding box containing all geo_point values for a
    * field.
    */
-  geoBounds: geoAggregation('geo_bounds'),
+  geoBounds: fieldAggregation('geo_bounds'),
   /**
    * A metric aggregation that computes the weighted centroid from all coordinate values for a
    * Geo-point datatype field.
    */
-  geoCentroid: geoAggregation('geo_centroid'),
-  terms: require('./terms'),
+  geoCentroid: fieldAggregation('geo_centroid'),
+  terms: fieldAggregation('terms'),
 };
--- src/elasticsearch/aggregations/terms.js
+++
@@ -1,8 +0,0 @@
-/**
- * A multi-bucket value source based aggregation where buckets are dynamically built - one per
- * unique value.
- */
-
-module.exports = field => ({
-  terms: { field },
-});

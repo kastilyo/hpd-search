36cef3ebfc4b2f9e9739e46ef65a9e839461a222
Mark Castillo <markalexandercastillo@gmail.com>
2019-03-17 09:20:51 -0400

    Account for case change

--- searchTest.js
+++ searchTest.js
@@ -9,8 +9,8 @@ const buildingRepository = axios.create({
 
 const buildingSearch = search({
   size: 10,
-  ...search.query(queries.bool({
-    ...queries.bool.filter(
+  ...search.query(Queries.bool({
+    ...Queries.bool.filter(
       building.clauses.inBrooklyn(),
       building.clauses.hasViolations(),
       building.clauses.hasComplaints(),
@@ -19,27 +19,27 @@ const buildingSearch = search({
     )
   })),
   ...search.aggs(
-    building.aggregations.centroid(),
-    building.aggregations.viewport(),
-    aggregations.terms('borough', 'borough', 1000),
-    aggregations.terms('censusTract', 'censusTract', 1000),
-    aggregations.terms('class', 'class', 1000),
-    aggregations.terms('communityBoard', 'communityBoard', 1000),
-    aggregations.terms('management', 'management', 1000),
-    aggregations.terms('postalCode', 'postalCode', 1000),
-    aggregations.terms('status', 'status', 1000),
-    aggregations.nested(
+    building.Aggregations.centroid(),
+    building.Aggregations.viewport(),
+    Aggregations.terms('borough', 'borough', 1000),
+    Aggregations.terms('censusTract', 'censusTract', 1000),
+    Aggregations.terms('class', 'class', 1000),
+    Aggregations.terms('communityBoard', 'communityBoard', 1000),
+    Aggregations.terms('management', 'management', 1000),
+    Aggregations.terms('postalCode', 'postalCode', 1000),
+    Aggregations.terms('status', 'status', 1000),
+    Aggregations.nested(
       'complaints',
-      aggregations.terms('status', 'complaints.status', 1000),
-      aggregations.nested(
+      Aggregations.terms('status', 'complaints.status', 1000),
+      Aggregations.nested(
         'complaints.problems',
-        aggregations.terms('code', 'complaints.problems.code', 1000),
-        aggregations.terms('majorCategory', 'complaints.problems.majorCategory', 1000),
-        aggregations.terms('minorCategory', 'complaints.problems.minorCategory', 1000),
-        aggregations.terms('spaceType', 'complaints.problems.spaceType', 1000),
-        aggregations.terms('status', 'complaints.problems.status', 1000),
-        aggregations.terms('type', 'complaints.problems.type', 1000),
-        aggregations.terms('unitType', 'complaints.problems.unitType', 1000),
+        Aggregations.terms('code', 'complaints.problems.code', 1000),
+        Aggregations.terms('majorCategory', 'complaints.problems.majorCategory', 1000),
+        Aggregations.terms('minorCategory', 'complaints.problems.minorCategory', 1000),
+        Aggregations.terms('spaceType', 'complaints.problems.spaceType', 1000),
+        Aggregations.terms('status', 'complaints.problems.status', 1000),
+        Aggregations.terms('type', 'complaints.problems.type', 1000),
+        Aggregations.terms('unitType', 'complaints.problems.unitType', 1000),
       ),
     ),
   )
@@ -51,6 +51,6 @@ jsonPrettify(buildingSearch);
 
 buildingRepository({
   data: buildingSearch,
-}).then(response => jsonPrettify(response.data.aggregations))
+}).then(response => jsonPrettify(response.data.Aggregations))
   .catch(e => jsonPrettify(e.response.data.error));
 
